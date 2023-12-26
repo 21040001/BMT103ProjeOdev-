0f351936ebf3ff44a0409a82174644fcf08543c1
DAVRONBEK ABDURAZZOQOV <94719744+21040001@users.noreply.github.com>
2023-12-26 23:40:07 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void ekle();
+void guncelle();
+void gorevler();
+int main(){
+    int a;
+    while (1){
+    printf("*Isleminizi Seciniz*\n" );
+    printf("1 - Plan Ekle\n");
+    printf("2 - Plan Guncelle\n");
+    printf("3 - Plani Sil\n");
+    printf("4 - Plani Goruntule\n");
+    printf("5 - Programi bitir\n");
+    scanf("%d", &a);
+
+    switch (a) {
+        case 1:
+            ekle();
+            break;
+        case 2:
+            guncelle();
+            break;
+        case 3:
+
+            break;
+        case 4:
+            gorevler();
+            break;
+        case 5:
+            return 0;
+        default:
+            printf("Gecersiz deger girdiniz !!!!!\n");
+            break;
+    }
+    }
+
+}
