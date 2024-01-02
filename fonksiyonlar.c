c3c4110ac02c0c81bebcc7b40161ba6c6de53b53
DAVRONBEK ABDURAZZOQOV <94719744+21040001@users.noreply.github.com>
2024-01-02 23:38:53 +0300

    Update fonksiyonlar.c
    Fonksiyonlar yeniden kontrol edilerek eksikler tamamlandi

--- fonksiyonlar.c
+++ fonksiyonlar.c
@@ -3,6 +3,7 @@
 #include <process.h>
 #include <stdlib.h>
 
+typedef struct File File;
 struct gorev{
     int saat;
     int dakika;
@@ -14,39 +15,59 @@ void ekle(){
     char get_ss[30];
     fileAdres = fopen("eylemler.txt","a+");
 
-    printf("Lutfen Eylem saati giriniz!! \n");
+    printf("Lutfen gorev saati giriniz!! \n");
     scanf("%d", &gorev1.saat);
-    printf("Lutfen Eylem dakikasini giriniz!! \n");
+    printf("Lutfen gorev dakikasini giriniz!! \n");
     scanf("%d", &gorev1.dakika);
-    printf("Lutfen Eylemi giriniz!!  \n");
+    printf("Lutfen gorevi giriniz!!  \n");
 
     scanf("%s",gorev1.eylem);
 
     if (fileAdres == NULL){
         printf("Dosya olusturulamadi, uzgunuz !!");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
     fprintf(fileAdres," %d:%d => %s \n ",gorev1.saat,gorev1.dakika,gorev1.eylem);
     fclose(fileAdres);
 
 }
-void guncelle(){
-    FILE *dosya;
-    int saat,dakika;
-    struct gorev gorev1;
-    dosya = fopen("eylemler.txt", "r");
-    if (dosya == NULL)
-    {
-        printf("Dosya acilamadi.\n");
+void guncelle() {
+    FILE *geciciDosya = fopen("dosya.txt", "w");
+    FILE *dosya = fopen("eylemler.txt", "r");
+    if (dosya == NULL) {
+        printf("Dosya acilamadi!");
         exit(1);
     }
+    int yenisaat, yenidakika;
+    char yenieylem[100];
+    char satir[100];
+    int saat, dakika;
+    char yeni_eylem[50];
 
-    printf("Lutfen guncellemek istediginiz Eylem saati giriniz!! \n");
+    printf("Lutfen degistirmek istediginiz gorevin saatini girin:\n ");
     scanf("%d", &saat);
-    printf("Lutfen guncellemek istediginiz Eylem dakikasini giriniz!! \n");
+    printf("Lutfen degistirmek istediginiz gorevin dakikasini girin:\n ");
     scanf("%d", &dakika);
-    fscanf(dosya, "%d %d",saat, dakika);
+
+    printf("Lutfen yeni gorev girin:\n ");
+    scanf("%s", yeni_eylem);
+
+    while (fgets(satir, sizeof(satir), dosya) != NULL) {
+        sscanf(satir, "%d:%d => %s\n]", &yenisaat, &yenidakika, yenieylem);
+        if (saat == yenisaat && dakika == yenidakika) {
+           fprintf(geciciDosya, "%d:%d => %s\n", saat, dakika, yeni_eylem);
+        } else {
+           fprintf(geciciDosya, "%d:%d => %s\n", yenisaat, yenidakika, yenieylem);
+        }
+    }
+
+    fclose(geciciDosya);
     fclose(dosya);
+
+    remove("eylemler.txt");
+    rename("dosya.txt", "eylemler.txt");
+
+    printf("gorev basariyla degistirildi!\n");
 }
 void gorevler(){
     char c;
@@ -54,7 +75,7 @@ void gorevler(){
     if (fileAdres == NULL)
     {
         printf("Dosya açılamadı!\n");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
     c = fgetc(fileAdres);
     while (c != EOF)
@@ -63,5 +84,14 @@ void gorevler(){
         c = fgetc(fileAdres);
     }
     fclose(fileAdres);
+}
 
+void silme(){
+   fileAdres = fopen("eylemler.txt", "w");  // dosyayı yazma momutuyla açtım
+    if(fileAdres != NULL){
+        fclose(fileAdres); // dosyayı kapattığımda dosya içeriği silinir
+        printf("Gorevler silindi \n");
+    }
+    else
+        printf("Dosya acilamadi\n"); // dosya açılmazsa bu hatra mesaj ekrana yazılacak
 }
