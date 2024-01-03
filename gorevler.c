db2ca73935a9e40586362b9def3771cb026c96ba
RIZA23171618008 <148904383+RIZA23171618008@users.noreply.github.com>
2024-01-03 11:46:03 +0300

    Update gorevler.c

--- gorevler.c
+++ gorevler.c
@@ -1,16 +1,16 @@
 void gorevler(){
     char c;
-    fileAdres = fopen("eylemler.txt", "r");
+    fileAdres = fopen("eylemler.txt", "r");//eylemler dosyasını açtım
     if (fileAdres == NULL)
     {
         printf("Dosya açılamadı!\n");
         exit(1);
-    }
-    c = fgetc(fileAdres);
+    }//dosya boşsa bunu bize bildiriyor
+    c = fgetc(fileAdres);//eylemler dosyasındaki verileri değişkene atadım
     while (c != EOF)
     {
         printf ("%c", c);
         c = fgetc(fileAdres);
-    }
-    fclose(fileAdres);
+    }//değikendeki verileri döngü yardımı ile ekrana yazdırdım
+    fclose(fileAdres);//dosyayı kapattım
 }
