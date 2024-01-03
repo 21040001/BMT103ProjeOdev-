6813a4ca7cda6a863114dc97bcd819c47213ef89
RIZA23171618008 <148904383+RIZA23171618008@users.noreply.github.com>
2024-01-03 11:45:11 +0300

    Update ekle.c

--- ekle.c
+++ ekle.c
@@ -1,21 +1,21 @@
 FILE * fileAdres;
 void ekle(){
     struct gorev gorev1;
-    char get_ss[30];
-    fileAdres = fopen("eylemler.txt","a+");
+    fileAdres = fopen("eylemler.txt","a+");//eylemler dosyasını açtım
 
-    printf("Lutfen Eylem saati giriniz!! \n");
+    printf("Lutfen gorev saati giriniz!! \n");
     scanf("%d", &gorev1.saat);
-    printf("Lutfen Eylem dakikasini giriniz!! \n");
+    printf("Lutfen gorev dakikasini giriniz!! \n");
     scanf("%d", &gorev1.dakika);
-    printf("Lutfen Eylemi giriniz!!  \n");
+    printf("Lutfen gorevi giriniz!!  \n");
 
-    scanf("%s",gorev1.eylem);
+    scanf("%s",gorev1.eylem);//değişkenlere değer atadım
 
     if (fileAdres == NULL){
         printf("Dosya olusturulamadi, uzgunuz !!");
-        exit(EXIT_FAILURE);
-    }
-    fprintf(fileAdres," %d:%d => %s \n ",gorev1.saat,gorev1.dakika,gorev1.eylem);
-    fclose(fileAdres);
+        exit(1);
+    }//dosya boş ise bildiriyor
+    fprintf(fileAdres," %d:%d => %s \n ",gorev1.saat,gorev1.dakika,gorev1.eylem);//değişkenleri eylemler dosyasının içine yazdırdım
+    fclose(fileAdres);//dosyayı kapattım
+
 }
