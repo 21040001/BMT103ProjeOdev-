724e7d6f687f6fa79f74387fc5627ffbabadd3bc
Mehmet Fatih Ekici <146962934+mfatihekici@users.noreply.github.com>
2024-01-03 09:10:36 +0300

    Update fonksiyonlar.c

--- fonksiyonlar.c
+++ fonksiyonlar.c
@@ -87,11 +87,11 @@ void gorevler(){
 }
 
 void silme(){
-   fileAdres = fopen("eylemler.txt", "w");  // dosyayı yazma momutuyla açtım
+   fileAdres = fopen("eylemler.txt", "w");  // dosyayı yazma komutuyla açtım
     if(fileAdres != NULL){
         fclose(fileAdres); // dosyayı kapattığımda dosya içeriği silinir
         printf("Gorevler silindi \n");
     }
     else
-        printf("Dosya acilamadi\n"); // dosya açılmazsa bu hatra mesaj ekrana yazılacak
+        printf("Dosya acilamadi\n"); // dosya açılmazsa bu mesaj ekrana yazılacak
 }
