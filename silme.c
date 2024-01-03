778292ba95d560044720b0da255e41c3bc4c25d2
RIZA23171618008 <148904383+RIZA23171618008@users.noreply.github.com>
2024-01-03 11:46:32 +0300

    Update silme.c

--- silme.c
+++ silme.c
@@ -1,9 +1,9 @@
 void silme(){
-   fileAdres = fopen("eylemler.txt", "w");  // dosyayı yazma komutuyla açtım
+   fileAdres = fopen("eylemler.txt", "w");  // dosyayı yazma momutuyla açtım
     if(fileAdres != NULL){
         fclose(fileAdres); // dosyayı kapattığımda dosya içeriği silinir
         printf("Gorevler silindi \n");
     }
     else
-        printf("Dosya acilamadi\n"); // dosya açılmazsa bu mesaj ekrana yazılacak
+        printf("Dosya acilamadi\n"); // dosya açılmazsa bu hatra mesaj ekrana yazılacak
 }
